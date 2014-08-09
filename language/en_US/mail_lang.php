f22f73a4957a5555a2c23a26348aa87d39cc941f
clearostranslations <translations@clearfoundation.com>
2014-08-09 00:40:21 -0400

    Added some common translation tags

--- language/en_US/mail_lang.php
+++ language/en_US/mail_lang.php
@@ -2,5 +2,10 @@
 
 $lang['mail_app_name'] = 'Mail Library';
 $lang['mail_app_description'] = 'The Mail app is a very simple library that handles core mail parameters for other mail apps.';
+$lang['mail_bounce'] = 'Bounce';
+$lang['mail_discard'] = 'Discard';
 $lang['mail_mail_domain'] = 'Mail Domain';
 $lang['mail_mail_domain_invalid'] = 'Mail domain is invalid.';
+$lang['mail_mail_domain_already_exists'] = 'Mail domain already exists.';
+$lang['mail_mail_hostname'] = 'Mail Hostname';
+$lang['mail_pass_through'] = 'Pass Through';
